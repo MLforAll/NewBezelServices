ec1ef6c253c17550a99522ca544ea04129616176
Kelian DUMARAIS <kdumarai@student.42.fr>
2020-03-06 17:54:35 +0100

    start writing real protocol

---
+++ XPC/OSDUIHelper_XPC_Protocol.h
@@ -0,0 +1,17 @@
+@protocol OSDUIHelperProtocol
+
+@required
+
+- (void)showImage:(long long)img onDisplayID:(CGDirectDisplayID)id priority:(unsigned int)prio msecUntilFade:(unsigned int)msec;
+
+- (void)showImage:(long long)img onDisplayID:(CGDirectDisplayID)id priority:(unsigned int)prio msecUntilFade:(unsigned int)msec withText:(NSString *)text;
+
+- (void)showImage:(long long)img onDisplayID:(CGDirectDisplayID)id priority:(unsigned int)prio msecUntilFade:(unsigned int) filledChiclets:(uint8_t)filled totalChiclets:(uint8_t)total locked:(uint8_t)locked;
+
+- (void)showImageAtPath:(NSString *path) onDisplayID:(CGDirectDisplayID)id priority:(unsigned int)prio msecUntilFade:(unsigned int)msec withText:(NSString *)text;
+
+- (void)fadeClassicImageOnDisplay:(CGDirectDisplayID)id;
+
+- (void)showFullScreenImage:(long long)img onDisplayID:(CGDirectDisplayID)id priority:(unsigned int)prio msecToAnimate:(unsigned int)msec;
+
+@end
